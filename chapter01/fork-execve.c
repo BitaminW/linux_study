611c4efe478f3982999df4658086ed54ab46857a
BitaminW <114838001+BitaminW@users.noreply.github.com>
2023-04-21 07:03:12 +0900

    Update fork-execve.c

--- chapter01/fork-execve.c
+++ chapter01/fork-execve.c
@@ -1,1 +1,9 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
 
+static void child(){
+    char *args[] = {"/bin/echo", "hello", NULL};
+    printf("I'm child1 my pid is %d.\n", getpid());
+}
